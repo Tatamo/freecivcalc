a4b7b2193ad2648c131e8a09671ab5b8ae009d44
Tatamo <nsorder@gmail.com>
2016-05-12 02:48:57 +0900

    restructure directories

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,15 @@ var rename = require("gulp-rename");
 var ejs = require("gulp-ejs");
 
 gulp.task("ejs", function(){
-	var json = JSON.parse(fs.readFileSync("./template/index_ja.json"));
-	gulp.src("./template/index.ejs")
+	var json = JSON.parse(fs.readFileSync("./src/template/index_ja.json"));
+	gulp.src("./src/template/index.ejs")
 	.pipe(plumber())
 	.pipe(ejs(json))
 	.pipe(rename("index.html"))
 	.pipe(gulp.dest('./'));
 
-	var json_en = JSON.parse(fs.readFileSync("./template/index_en.json"));
-	gulp.src("./template/index.ejs")
+	var json_en = JSON.parse(fs.readFileSync("./src/template/index_en.json"));
+	gulp.src("./src/template/index.ejs")
 	.pipe(plumber())
 	.pipe(ejs(json_en))
 	.pipe(rename("index_en.html"))
